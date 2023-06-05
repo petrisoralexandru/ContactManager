cbe86d1c278fad5b81015dc1a43a39792d4d026b
petrisoralexandru <kwixacademy@gmail.com>
2023-06-05 23:49:53 +0300

    Added GetConnectionString method to be able to read the connection string from the appsettings

--- UI/Program.cs
+++ UI/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace UI;
+﻿using Microsoft.Extensions.Configuration;
+
+namespace UI;
 
 public class Program
 {
     private static void Main()
     {
         
+    }
+    
+    private static string? GetConnectionString(string connectionStringName = "Default")
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json");
+
+        var config = builder.Build();
+
+        var output = config.GetConnectionString(connectionStringName);
+
+        return output;
     }
 }
